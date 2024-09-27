20b3ea4563175826ffd0a9d61ce073c77f71f398
Ignacio Olave <me@iolave.com>
2024-09-27 15:02:39 -0300

    added tab new and tab next shortcuts

--- init.lua
+++ init.lua
@@ -911,5 +911,8 @@ require('lazy').setup({
   },
 })
 
+vim.keymap.set('n', '<leader>ptt', ':tabnew<cr>')
+vim.keymap.set('n', '<leader>ptn', ':tabNext<cr>')
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
