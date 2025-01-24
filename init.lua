7f54a36185f2adf409a1c7e4a0c360828786f970
Ignacio Olave <me@iolave.com>
2025-01-24 11:54:13 -0300

    adds presence plugin and disables deno

--- init.lua
+++ init.lua
@@ -295,7 +295,35 @@ require('lazy').setup({
       }, { mode = 'v' })
     end,
   },
+  {
+    'andweeb/presence.nvim',
+    config = function()
+      -- The setup config table shows all available config options with their default values:
+      require('presence').setup {
+        -- General options
+        auto_update = true, -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
+        neovim_image_text = 'meh', -- Text displayed when hovered over the Neovim image
+        main_image = 'neovim', -- Main image display (either "neovim" or "file")
+        --client_id = '793271441293967371', -- Use your own Discord application client id (not recommended)
+        log_level = nil, -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
+        debounce_timeout = 10, -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
+        enable_line_number = false, -- Displays the current line number instead of the current project
+        blacklist = {}, -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
+        buttons = true, -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
+        file_assets = {}, -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
+        show_time = true, -- Show the timer
 
+        -- Rich Presence text options
+        editing_text = 'Editing %s', -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
+        file_explorer_text = 'Browsing %s', -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
+        git_commit_text = 'Committing changes', -- Format string rendered when committing changes in git (either string or function(filename: string): string)
+        plugin_manager_text = 'Managing plugins', -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
+        reading_text = 'Reading %s', -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
+        workspace_text = 'Working on %s', -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
+        line_number_text = 'Line %s out of %s', -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
+      }
+    end,
+  },
   -- NOTE: Plugins can specify dependencies.
   --
   -- The dependencies are proper plugin specifications as well - anything
@@ -575,8 +603,8 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
-        denols = {},
+        ts_ls = {},
+        -- denols = {},
         --
         terraformls = {},
 
@@ -596,17 +624,17 @@ require('lazy').setup({
         },
       }
 
-      local nvim_lsp = require 'lspconfig'
-      nvim_lsp.denols.setup {
-        on_attach = on_attach,
-        root_dir = nvim_lsp.util.root_pattern('deno.json', 'deno.jsonc'),
-      }
+      -- local nvim_lsp = require 'lspconfig'
+      -- nvim_lsp.denols.setup {
+      --   on_attach = on_attach,
+      --   root_dir = nvim_lsp.util.root_pattern('deno.json', 'deno.jsonc'),
+      -- }
 
-      nvim_lsp.tsserver.setup {
-        on_attach = on_attach,
-        root_dir = nvim_lsp.util.root_pattern 'package.json',
-        single_file_support = false,
-      }
+      -- nvim_lsp.ts_ls.setup {
+      --   on_attach = on_attach,
+      --   root_dir = nvim_lsp.util.root_pattern 'package.json',
+      --   single_file_support = false,
+      -- }
 
       -- Ensure the servers and tools above are installed
       --  To check the current status of installed tools and/or manually install
